65491f1c0010a9cf2242be701da0fc205c1bdbf0
Voron-1982 <os-kyokushin1@rambler.ru>
2024-06-26 21:26:36 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+# immutable_var = 1, 2, 'star', True
+# print(immutable_var)
+# immutable_var[0] = 3
+# print(immutable_var) # кортеж это неизменяемы списки, созданы для сохранения данных в первоначальном виде
+mutable_list = [1, 2, 3, 'finish', False]
+print(mutable_list)
+mutable_list[2] = 'string'
+print(mutable_list)
